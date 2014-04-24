42dedd39d99752cb5c9b0c88b25016f34a671fd2
Konstantin Sergeev <k.sergeev@itdhq.com>
2014-04-25 00:40:00 +0400

    Placeholder for alvex-header-menu implemented as an extension

---
+++ core/share-generic-ui/4.2.d/share/config/alfresco/site-webscripts/alvex/header-menu/share-header.get.js
@@ -0,0 +1,16 @@
+<import resource="classpath:alfresco/web-extension/site-webscripts/header/share-header.lib.js">
+
+model.jsonModel = {
+   rootNodeId: "share-header",
+   services: getHeaderServices(),
+   widgets: [
+      {
+         id: "SHARE_VERTICAL_LAYOUT",
+         name: "alfresco/layout/VerticalWidgets",
+         config: 
+         {
+            widgets: getHeaderModel()
+         }
+      }
+   ]
+};
