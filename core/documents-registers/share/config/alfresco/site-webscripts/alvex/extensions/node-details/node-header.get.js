41f087f773b9a35638c8fa8e6d338fffe5e6a40d
Mikhail Khokhlov <mkhokhlov@itdhq.com>
2014-04-30 13:39:48 +0400

    Implement ALV-668

--- core/documents-registers/share/config/alfresco/site-webscripts/alvex/extensions/node-details/node-header.get.js
+++ core/documents-registers/share/config/alfresco/site-webscripts/alvex/extensions/node-details/node-header.get.js
@@ -2,4 +2,9 @@
 
 var documentDetails = AlfrescoUtil.getNodeDetails(model.nodeRef, model.site, null, model.libraryRoot)
 if( !documentDetails.item.node.contentURL )
+{
+	// ALV-665
 	model.showQuickShare = "false";
+	// ALV-668
+	model.showPath = "false";
+}
