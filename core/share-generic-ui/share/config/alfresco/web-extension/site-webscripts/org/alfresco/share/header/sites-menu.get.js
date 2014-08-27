76d91f020da8b7d1f3b623020708e29a1fbf182c
Mikhail Khokhlov <mkhokhlov@itdhq.com>
2014-08-27 17:55:25 +0400

    Fix ALV-677 (most probably, backporting fix for ALF-18436, that is not mentioned in Alfresco JIRA for some reason)

--- core/share-generic-ui/share/config/alfresco/web-extension/site-webscripts/org/alfresco/share/header/sites-menu.get.js
+++ core/share-generic-ui/share/config/alfresco/web-extension/site-webscripts/org/alfresco/share/header/sites-menu.get.js
@@ -9,16 +9,9 @@ function getSitesMenuData() {
        getRecentSites = (mode == "all" || mode == "recent");
    
    // Get the preferences for the current user...
-   var result = remote.call("/api/people/" + encodeURIComponent(user.name) + "/preferences"),
-       prefs,
-       recentSites,
-       favourites;
-   if (result.status == 200 && result != "{}")
-   {
-      prefs = eval('(' + result + ')');
-      recentSites = eval('try{(prefs.org.alfresco.share.sites.recent)}catch(e){}');
-      favourites = eval('try{(prefs.org.alfresco.share.sites.favourites)}catch(e){}');
-   }
+   var prefs = jsonUtils.toObject(preferences.value);
+   var recentSites = eval('try{(prefs.org.alfresco.share.sites.recent)}catch(e){}'),
+       favourites = eval('try{(prefs.org.alfresco.share.sites.favourites)}catch(e){}');
    // Check that recentSites and favourites have been initialised by the successful
    // response of requesting preferences. If not then just make them a new object and
    // this will be reflected in the UI as there being no recent sites or favourites.
@@ -156,11 +149,11 @@ function getSitesMenuData() {
    
    // Make the request to get the site metadata...
    var connector = remote.connect("alfresco");
-   result = connector.post("/api/sites/query", jsonUtils.toJSONString(query), "application/json");
+   var result = connector.post("/api/sites/query", jsonUtils.toJSONString(query), "application/json");
    if (result.status == 200)
    {
       // Create JavaScript objects from the server response
-      var sites = eval('(' + result + ')'), site;
+      var sites = JSON.parse(result), site;
       if (sites.length != 0)
       {
          // Iterate over the list and construct the favourites list...
@@ -232,6 +225,14 @@ function getSitesMenuData() {
    
    if (getRecentSites)
    {
+      // clean up the recent sites menu by removing gaps
+      for (var i=0; i<recentSiteMenuItems.length; i++)
+      {
+         if (recentSiteMenuItems[i] === undefined)
+         {
+            recentSiteMenuItems.splice(i--,1);
+         }
+      }
       sitesMenu.widgetsRecent = recentSiteMenuItems;
    }  
    if (getFavouriteSites)
